d14e9c6755638da504ace8880f72010f2eb7cbfe
FernandoAquinoPinheiro <144848529+FernandoAquinoPinheiro@users.noreply.github.com>
2023-09-13 16:47:53 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+# numero = 10
+# print (numero)
+
+# nome = input("digite o seu nome:")
+# print(nome)
+
+#text = "Hello world"
+#print(text)
+
+#comentario
+
+#variaveis do tipo logico
+
+# escolha = True
+# escolha2 = False
+# escolha = False
+# escolha = True
+# print(escolha)
+
+# import os
+# os.system ('cls')  // limpar
+
+# 1 crie um sistema que some o valor de 2 variaveis e imprima a soma
+
+# numero1 = int (input ("Digite um Valor"))
+# numero2 = int (input ("digite um segundo valor"))
+
+# resultado = numero1 + numero2
+
+# print("A soma desses valores é " , resultado)
+
+# 2 Crie um sistema que imprima seu nome na tela:
+
+# print("Digite seu nome")
+# nome = input ()
+# print("Seu nome é " ,nome )
+
+# 3 Crie um sistema que imprima seu nome e um comprimento;
+
+# print("Digite seu nome")
+# nome = input ()
+# print( "Olá " , nome, "Seja bem Vindo" )
+
+# 4 Crie um sistema que multiplique 2 numeros;
+
+print("digite um valor")
+valor1 = input()
+print("Digite um segundo valor")
+valor2 = input()
+
+resultado = valo1 * valor2
+
+print("A multiplcação desses dois Valores são: ",resultado,)
+
+
+
+
